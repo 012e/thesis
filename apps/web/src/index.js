6c504ae1dc3b2ce0de803cda17e7f0079aae6177
012e <huyphmnhat@gmail.com>
2026-02-13 17:49:16 +0700

    feat: initialize web app with TanStack Router

--- apps/web/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user = {
-    id: '1',
-    username: 'test',
-    email: 'test@example.com'
-};
-console.log(user);
