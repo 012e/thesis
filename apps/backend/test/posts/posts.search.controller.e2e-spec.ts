a57a6a528efe31c33de979ad42b375c2df730b53
012e <huyphmnhat@gmail.com>
2026-04-27 20:03:06 +0700

    fix comment counts not found

--- apps/backend/test/posts/posts.search.controller.e2e-spec.ts
+++ apps/backend/test/posts/posts.search.controller.e2e-spec.ts
@@ -149,6 +149,7 @@ describe("GET /posts/search", () => {
     expect(post.updatedAt).toMatch(/^\d{4}-\d{2}-\d{2}T/);
     expect(typeof post.upvoteCount).toBe("number");
     expect(typeof post.downvoteCount).toBe("number");
+    expect(typeof post.commentCount).toBe("number");
   });
 
   it("is case-insensitive", async () => {
